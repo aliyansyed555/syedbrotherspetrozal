be45181eb7d37cf934eba0e753e9f634ebd80710
Amir <Amirgee007@yahoo.com>
2025-02-13 17:17:53 +0500

    updated reports data

--- app/Http/Controllers/ClientAdmin/PetrolPumpController.php
+++ app/Http/Controllers/ClientAdmin/PetrolPumpController.php
@@ -1450,11 +1450,11 @@ class PetrolPumpController extends Controller
             nr.date,
             ft.id AS fuel_type_id,
             nr.digital_reading - COALESCE(
-                LAG(nr.digital_reading) OVER (PARTITION BY nr.nozzle_id ORDER BY nr.date),
+                LAG(nr.digital_reading) OVER (PARTITION BY nr.nozzle_id ORDER BY nr.date , nr.id),
                 digital_reading
             ) AS digital_sold_ltrs,
             nr.analog_reading - COALESCE(
-                LAG(nr.analog_reading) OVER (PARTITION BY nr.nozzle_id ORDER BY nr.date),
+                LAG(nr.analog_reading) OVER (PARTITION BY nr.nozzle_id ORDER BY nr.date , nr.id),
                 analog_reading
             ) AS analog_sold_ltrs,
             fr.selling_price,
